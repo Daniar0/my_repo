d61beadbaf8fa5ba24f88282b129f4c8f4e940b0
Daniar <dakimaliev@gmail.com>
2024-01-04 21:45:23 +0600

    first commit2

--- lesson1/Task5/Program.cs
+++ lesson1/Task5/Program.cs
@@ -19,11 +19,12 @@
 // {
 //     Console.WriteLine(b);
 // }
-int n=20;
-int i=1;
-while(i<=n)
-{
-    Console.Write(i);
-    Console.Write('');
-    i=i+1;
-}
+// int n=20;
+// int i=1;
+// while(i<=n)
+// {
+//     Console.Write(i);
+//     Console.Write(' ');
+//     i=i+1;
+// }
+
