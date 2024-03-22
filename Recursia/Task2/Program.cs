0cfa095da855d8992f6088fe7ea858ef39a96329
Daniar <dakimaliev@gmail.com>
2024-03-23 00:58:19 +0600

    add info about recursion

---
+++ Recursia/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 29
+
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else {
+        return Akkerman(m- 1, Akkerman(m, n- 1));
+    }
+}
+
+
+AkkermanFunction(2,3);
+
+
